3dc642bb9dd11260332750836f45f9ba87b0f4ca
Adrian Vargas <adrianfelipevr@gmail.com>
2025-03-31 14:48:42 -0500

    initial configurations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
-
+export class HomePage implements AfterViewInit {
   constructor() {}
 
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  initMap(): void {
+    const map = L.map('mapID').setView(
+      [4.60646885927244, -74.08135130116464],
+      13
+    ); // [Latitud, Longitud]
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 19,
+      }
+    );
+    tiles.addTo(map);
+  }
 }
