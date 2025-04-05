fa3c8c3b6b3713fe6d1e857b016c0b3cc94bc183
Adrian Vargas <adrianfelipevr@gmail.com>
2025-04-05 18:55:47 -0500

    add setPoint method.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
 import { Geolocation } from '@capacitor/geolocation';
 import { AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
@@ -9,18 +10,70 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
+
 export class HomePage implements AfterViewInit {
   private map!: L.Map;
+  private points: { name: string; coords: [number, number] }[] = [];
+  private routeInterval: any;
+  private points1: { name: string; coords: [number, number] }[] = [
+    { name: 'Centro de Bogotá', coords: [4.710989, -74.07209] },
+    { name: 'Chapinero', coords: [4.71585, -74.034686] },
+    { name: 'Teusaquillo', coords: [4.658646, -74.060854] },
+    { name: 'Kennedy', coords: [4.625461, -74.093309] },
+    { name: 'Engativá', coords: [4.682765, -74.119824] },
+    { name: 'Puente Aranda', coords: [4.64635, -74.059952] },
+    { name: 'Suba', coords: [4.738958, -74.100161] },
+    { name: 'La Candelaria', coords: [4.598272, -74.076375] },
+    { name: 'Usaquén', coords: [4.760684, -74.044926] },
+    { name: 'Antonio Nariño', coords: [4.635223, -74.081994] },
+  ];
+  private i = 0;
+  // Icono mapa
+  private customIconRed = L.icon({
+    iconUrl: './assets/icon/marker-map.png',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -40],
+  });
+  private customIconGreen = L.icon({
+    iconUrl: './assets/icon/marker-map-green.png',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -40],
+  });
+  // Punto por defecto (Estacion Bomberos Egipto)
+  private defaultPosition: [number, number] = [
+    4.592082775872901, -74.07004845194825,
+  ];
 
-  constructor(private alertController: AlertController) {}
+  constructor(
+    private _AlertController: AlertController,
+    private _Storage: Storage
+  ) {}
+
+  async ngOnInit() {
+    await this._Storage.create();
+
+    const points = await this._Storage.get('points');
+    this.points = points ? JSON.parse(points) : [];
+  }
 
   ngAfterViewInit(): void {
     this.initMap();
-    setTimeout(() => this.map.invalidateSize(), 500);
+    setTimeout(() => {
+      this.map.invalidateSize();
+      this.drawRoute();
+      console.log(this.points);
+    }, 500);
   }
+
+  private async savePoint() {
+    await this._Storage.set('points', JSON.stringify(this.points));
+  }
+
   // Mostrar el alert
   async showErrorAlert(e: string) {
-    const alert = await this.alertController.create({
+    const alert = await this._AlertController.create({
       header: 'Error',
       subHeader: 'Ubicación no encontrada',
       message: `Detalle: ${e}`,
@@ -32,39 +85,6 @@ export class HomePage implements AfterViewInit {
 
   // Iniciar el Mapa
   async initMap(): Promise<void> {
-    // Punto por defecto (Estacion Bomberos Egipto)
-    const defaultPosition: [number, number] = [
-      4.592082775872901, -74.07004845194825,
-    ];
-    // Icono mapa
-    const customIconRed = L.icon({
-      iconUrl: './assets/icon/marker-map.png',
-      iconSize: [40, 40],
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40],
-    });
-    const customIconGreen = L.icon({
-      iconUrl: './assets/icon/marker-map-green.png',
-      iconSize: [40, 40],
-      iconAnchor: [20, 40],
-      popupAnchor: [0, -40],
-    });
-    // Arreglo de Puntos
-    const points = [];
-
-    const puntos = [
-      { name: 'Centro de Bogotá', coords: [4.710989, -74.07209] },
-      { name: 'Chapinero', coords: [4.71585, -74.034686] },
-      { name: 'Teusaquillo', coords: [4.658646, -74.060854] },
-      { name: 'Kennedy', coords: [4.625461, -74.093309] },
-      { name: 'Engativá', coords: [4.682765, -74.119824] },
-      { name: 'Puente Aranda', coords: [4.64635, -74.059952] },
-      { name: 'Suba', coords: [4.738958, -74.100161] },
-      { name: 'La Candelaria', coords: [4.598272, -74.076375] },
-      { name: 'Usaquén', coords: [4.760684, -74.044926] },
-      { name: 'Antonio Nariño', coords: [4.635223, -74.081994] },
-    ];
-
     try {
       // Solicitar permisos de geolocalización
       const permission = await Geolocation.requestPermissions();
@@ -76,7 +96,7 @@ export class HomePage implements AfterViewInit {
 
       this.map = L.map('mapID').setView([latitude, longitude], 18);
 
-      L.marker([latitude, longitude], { icon: customIconRed })
+      L.marker([latitude, longitude], { icon: this.customIconRed })
         .addTo(this.map)
         .bindPopup("I'm here!!!")
         .openPopup();
@@ -85,30 +105,88 @@ export class HomePage implements AfterViewInit {
         error.message || 'No se pudo establecer la ubicacion.'
       );
 
-      this.map = L.map('mapID').setView(defaultPosition, 18);
+      this.map = L.map('mapID').setView(this.defaultPosition, 18);
 
-      L.marker(defaultPosition, { icon: customIconRed })
+      L.marker(this.defaultPosition, { icon: this.customIconRed })
         .addTo(this.map)
         .bindPopup('Bomberos Egipto')
         .openPopup();
     }
-
-    const ruta = L.polyline(
-      puntos.map((punto) => punto.coords as [number, number]),
-      { color: 'blue' }
-    ).addTo(this.map);
-    this.map.fitBounds(ruta.getBounds());
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors',
     }).addTo(this.map);
+  }
 
-    puntos.forEach((punto) => {
-      L.marker(punto.coords as [number, number], { icon: customIconGreen })
+  startRoute(): void {
+    this.points.push({
+      name: `Start Point`,
+      coords: this.defaultPosition,
+    });
+    this.routeInterval = setInterval(async () => {
+      try {
+        const position = await Geolocation.getCurrentPosition();
+        const { latitude, longitude } = position.coords;
+
+        this.points.push({
+          name: `Point ${this.points.length + 1}`,
+          coords: [latitude, longitude],
+        });
+
+        await this.savePoint();
+        this.drawRoute();
+
+        if (this.points.length >= 40) clearInterval(this.routeInterval);
+      } catch (error: any) {
+        this.points.push({
+          name: `Point ${this.points.length + 1}`,
+          coords: this.points1[this.i].coords,
+        });
+        this.i++;
+        await this.savePoint();
+        this.drawRoute();
+        if (this.points.length == this.points1.length)
+          clearInterval(this.routeInterval);
+      }
+    }, 5000);
+  }
+
+  drawRoute(): void {
+    if (!this.map || this.points.length === 0) return;
+    L.polyline(
+      this.points.map((point) => point.coords as [number, number]),
+      { color: 'blue' }
+    )
+      .addTo(this.map)
+      .bringToFront();
+    // Ajustar el mapa a los límites de la ruta
+    this.map.fitBounds(
+      this.points.map((point) => point.coords as [number, number])
+    );
+    this.points.forEach((point) => {
+      L.marker(point.coords as [number, number], {
+        icon: this.customIconGreen,
+      })
         .addTo(this.map)
-        .bindPopup(punto.name)
+        .bindPopup(point.name)
         .openPopup();
     });
+  }
+
+  async setPoint(): Promise<void> {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      const { latitude, longitude } = position.coords;
+
+      this.points.push({
+        name: `Point ${this.points.length + 1}`,
+        coords: [latitude, longitude],
+      });
+
+      await this.savePoint();
+      this.drawRoute();
+    } catch (error) {
+      console.log('No se pudo colocar el punto.');
+    }
   }
 
   ngOnDestroy(): void {
